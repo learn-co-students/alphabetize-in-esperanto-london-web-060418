14fe64b0563397b93e260288a690f4e5ee1fdc79
Jigar Polra <jigarpolra@live.co.uk>
2018-05-29 17:01:08 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+require "pry"
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  split_array = ESPERANTO_ALPHABET.split("")
+  sortedArray = arr.sort { |a, b| a.split("").map { |char| split_array.find_index(char) } <=> b.split("").map { |char| split_array.find_index(char) } }
+  return sortedArray
 end
